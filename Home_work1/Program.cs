d71081e4749d508b7618ca04453a2a4a5a157916
Alexei <vasay490@mail.ru>
2022-04-19 23:16:15 +0300

    выполнил 2 задачи

--- Home_work1/Program.cs
+++ Home_work1/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.Write("input first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1 ;
+
+if (num1 > num2)
+
+{
+    Console.WriteLine(" max number1");
+}
+else 
+{
+    Console.WriteLine(" max number2");
+}
+*/
+
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.Write("input first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input the third integer number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int a = num1 ;
+int b = num2 ;
+int c = num3 ;
+
+int max = a;
+
+if (a > max) max = num1;
+if (b > max) max = num2;
+if (c > max) max = num3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
+
+// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
