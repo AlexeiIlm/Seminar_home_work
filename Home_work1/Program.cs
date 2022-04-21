5adb5a195f57716ae898a3aecfa361f297c8023a
Alexei <vasay490@mail.ru>
2022-04-21 22:59:14 +0300

    выполнил 4 задачу, нашел похожий код в поиске и его изменил

--- Home_work1/Program.cs
+++ Home_work1/Program.cs
@@ -44,7 +44,7 @@ Console.WriteLine(max);
 // Задача 3: Напишите программу, которая на вход принимает число и выдаёт, 
 // является ли число чётным (делится ли оно на два без остатка).
 
-
+/*
 Console.Write("input even number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -60,25 +60,22 @@ else
     Console.WriteLine(" odd number");
 }
 
-
+*/
 
 // Задача 4: Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N.
 
+
 /*
-Console.Write("input even number: ");
+Console.Write("input second integer number: ");
 int num = Convert.ToInt32(Console.ReadLine());
-
-int result = num;
-int num2 = 2;
-
-if (result == num/num2 )
-
-{
-    Console.WriteLine(" even number");
-}
-else 
-{
-    Console.WriteLine(" odd number");
-}
-*/
+        for (int i =1; i <= num; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
+        */
+        
