d8597b9e8442c8f6efb66da8d1a0daf5faabe549
AlexIlm <103334393+AlexeiIlm@users.noreply.github.com>
2022-04-26 23:45:41 +0300

    Add files via upload

--- Home_work2/Program.cs
+++ Home_work2/Program.cs
@@ -1,3 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-new
+﻿//  Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает вторую цифру этого числа.
+/*
+void method_one ()
+{
+     Console.Write("three-digit number: ");
+     int num = Convert.ToInt32(Console.ReadLine());
+     {
+        Console.WriteLine(num>99 ? num.ToString()[1] : '-');
+        Console.ReadKey(true);
+
+     }
+   
+      Console.Write(num);
+    
+}
+method_one();
+
+*/
+
+//Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+/*
+void method_two ()
+{
+     Console.Write("three-digit number: ");
+     int num = Convert.ToInt32(Console.ReadLine());
+     if(num> 100)
+     {
+        Console.WriteLine(num>99 ? num.ToString()[2] : '-');
+        Console.ReadKey(true);
+     }
+     else
+     {
+         Console.WriteLine(0);
+     }
+   
+      Console.Write(num);
+    
+}
+method_two();
+*/
+
+//Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+void DayOFF ()
+{
+     Console.Write("day of the week number: ");
+     int num = Convert.ToInt32(Console.ReadLine());
+
+     int day = num;
+
+     if (num<6 & num>1)
+     {
+         Console.WriteLine(" working day");
+         
+     }
+     else
+     {
+         Console.WriteLine(" day off");
+        
+     }
+}
+
+DayOFF ();
+*/
+
+//26/04/2022
+
+
+     
